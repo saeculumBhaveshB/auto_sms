b6dee19a0b6cc9e7eca7d856b205c0c679c4e39d
BhaveshBarotSS <bhavesh.barot@saeculumsolutions.com>
2025-06-05 12:36:45 +0530

    Update index.ts

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,4 +1,3 @@
-export { default as AutoSmsStatusScreen } from "./AutoSmsStatusScreen";
 export { default as PermissionsStatusScreen } from "./PermissionsStatusScreen";
 export { default as AIDocumentScreen } from "./AIDocumentScreen";
 export { default as AIChatLogScreen } from "./AIChatLogScreen";
